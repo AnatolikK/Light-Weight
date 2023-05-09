d58c8796609aa25b3f18f794b400543e420eb01d
vladlen32230 <122670848+vladlen32230@users.noreply.github.com>
2023-05-09 14:56:40 +0500

    Add files via upload

---
+++ CommentaryInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace Project
+{
+    public class CommentaryInfo
+    {
+        public string AuthorName { get; set; }
+        public string Text { get; set; }
+        public string CreationDate { get; set; }
+        public bool IsOwner { get; set; }
+        public int Id { get; set; }
+    }
+}
